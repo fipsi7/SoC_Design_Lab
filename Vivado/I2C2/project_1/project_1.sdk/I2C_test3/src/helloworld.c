0b9c1303fc1d0446e6233d217d03ebde215c2de2
EberhartEberhart <florian.egert@gmx.at>
2022-02-19 10:14:38 +0100

    Small fixes for UART and visualization

--- Vivado/I2C2/project_1/project_1.sdk/I2C_test3/src/helloworld.c
+++ Vivado/I2C2/project_1/project_1.sdk/I2C_test3/src/helloworld.c
@@ -58,8 +58,9 @@ int readBytes(int device, int i2c_addr, uint8_t adr, uint8_t result[], uint8_t c
 int transRecvBytes(int device, int i2c_addr, uint8_t sendBuf[], uint8_t sendCnt, uint8_t receiveBuf[], uint8_t receiveCount, uint8_t protocol);
 void transmitBytes(int device, int i2c_addr, uint8_t sendBuf[], uint8_t sendCnt, Xuint8 protocol);
 int receiveBytes(int device, int i2c_addr, uint8_t receiveBuf[], uint8_t cnt, Xuint8 protocol);
-int I2C_WriteByte(int device, Xuint8 addr, Xuint8 data);
-int I2C_ReadByte(int device, Xuint8 addr, Xuint8 *data);
+void red(void);
+void resetcol(void);
+void green(void);
 
 /************************** Variable Definitions *****************************/
 
@@ -67,129 +68,153 @@ int I2C_ReadByte(int device, Xuint8 addr, Xuint8 *data);
  * FD of the IIC device opened.
  */
 int Fd;
-int Fdiic;
-int Fduart;
 
 
 /**************************** Function Definitions ***************************/
 
-/*****************************************************************************/
-/**
-*
-* Entry point for integration tests on IIC.
-*
-* @param    .
-*
-* @return   0 if successful else -1.
-*
-* @note     None.
-*
-******************************************************************************/
+
 int main()
 {
-	Xuint8 Buffer[30];
+	Xuint8 Buffer[30]; // Send and receive buffer
 	int i;
 	int protocol = 1; //0: I2C, 1: UART
-	int found = 0;
-	char bitnames[2][32]={"I2C.bit", "UART9600.bit"};
+	int found = 0; // Flag indicating detected protocol
+	char bitnames[2][32]={"I2C.bit", "UART9600.bit"}; // Bit-Files for dyn. rec.of PL
+	GPIO buttons;
+	GPIO leds;
 
-	system("stty -F /dev/ttyUL1 -echo");
+	// LED and Button configuration
+	buttons = GPIO_init(0, 0);
+	leds = GPIO_init(1, 0);
+	setPinMode(leds,1,1,0);
+	setPinMode(leds,1,2,0);
+	setPinMode(leds,1,3,0);
+	setPinMode(leds,1,4,0);
+
+	// UART driver configuration
+	system("stty -F /dev/ttyUL1 -echo raw");
 	usleep(10000);
 
-	while(!found)
+
+	while(!digitalRead(buttons, 2, 1)) // Please press BTNC to exit
 	{
-		protocol = (protocol + 1) % 2;
+		usleep(1000000);
 
-		if(flash_bitfile(bitnames[protocol]))
-			return 1;
+		// Search for protocol until detected
+		while(!found)
+		{
 
-		if(protocol == 1)
-		{
-			Fd = open("/dev/ttyUL1", O_RDWR);
-		}
-		else
-		{
-			Fd = open("/dev/i2c-0", O_RDWR);
-		}
-
-		if(Fd < 0)
-		{
-			printf("Cannot open file descriptor\n");
-			return 1;
-		}
-
-		if(!readBytes(Fd,0x39, 0x00, Buffer, 3, protocol))
-		{
-			if((Buffer[0] == 'T') && (Buffer[1] == 'U') && (Buffer[2] == 'W'))
+			// Exit button pressed
+			if(digitalRead(buttons, 2, 1))
 			{
-				found = 1;
-				printf("Protocol %s found!\n", protocol == 1 ? "UART" : "I2C");
+				GPIO_Close(leds);
+				GPIO_Close(buttons);
+				close(Fd);
+				printf("\nreturn 0\n\n");
+				return 0;
+			}
+
+			// Switch protocol every try
+			protocol = (protocol + 1) % 2;
+
+			printf("\n");
+
+			// DYNAMIC RECONFIGURATION to next protocol
+			if(flash_bitfile(bitnames[protocol]))
+			{
+				GPIO_Close(leds);
+				GPIO_Close(buttons);
+				close(Fd);
+				printf("\nreturn 1\n\n");
+				return 1;
+			}
+
+			// Open file descriptor
+			if(protocol == 1)
+			{
+				Fd = open("/dev/ttyUL1", O_RDWR);
+			}
+			else
+			{
+				Fd = open("/dev/i2c-0", O_RDWR);
+			}
+			if(Fd < 0)
+			{
+				printf("Cannot open file descriptor\n");
+				{
+					GPIO_Close(leds);
+					GPIO_Close(buttons);
+					close(Fd);
+					printf("\nreturn 1\n\n");
+					return 1;
+				}
+			}
+
+			// Detection of id "TUW" (address 0x00), I2C address: 0x39
+			if(!readBytes(Fd,0x39, 0x00, Buffer, 3, protocol))
+			{
+				if((Buffer[0] == 'T') && (Buffer[1] == 'U') && (Buffer[2] == 'W'))
+				{
+					found = 1;
+					green();
+					printf("\nProtocol %s found!\n", protocol == 1 ? "UART" : "I2C");
+					resetcol();
+				}
 			}
 		}
+		found = 0; // Reset flag if not found
+
+		// Set protocol LEDs
+		digitalWrite(leds,1,7,protocol);
+		digitalWrite(leds,1,8,!protocol);
+
+		usleep(1000000);
+
+		// Continuously receive test data
+		while(!readBytes(Fd,0x39, 0x03, Buffer, 1, protocol))
+		{
+
+			// Exit button
+			if(digitalRead(buttons, 2, 1))
+			{
+				GPIO_Close(leds);
+				GPIO_Close(buttons);
+				close(Fd);
+				printf("\nreturn 0\n\n");
+				return 0;
+			}
+
+			// Visualize received value
+			Buffer[0]-=0x61;
+			printf("\n%d", Buffer[0]);
+			digitalWrite(leds,1,1,(Buffer[0]>>0x00)&0x01);
+			digitalWrite(leds,1,2,(Buffer[0]>>0x01)&0x01);
+			digitalWrite(leds,1,3,(Buffer[0]>>0x02)&0x01);
+			digitalWrite(leds,1,4,(Buffer[0]>>0x03)&0x01);
+
+			usleep(100000);
+		}
+
+		close(Fd);
 	}
-	close(Fd);
-
-	/*flash_bitfile(i2c_name);
-
-	Fdiic = open("/dev/i2c-0", O_RDWR);
-	if(Fdiic < 0)
-	{
-		printf("Cannot open the IIC device\n");
-		return -1;
-	}
-
-	readBytes(Fdiic,0x39, 0x00, Buffer, 3, 0);
-	for(i = 0; i<3; i++)
-	{
-		printf("%c", Buffer[i]);
-	}
-
-	for(i=0; i<20; i++)
-	{
-		readBytes(Fdiic,0x39, 0x03, Buffer, 1, 0);
-		printf("\n%d", Buffer[0]);
-		usleep(100000);
-	}
-	close(Fdiic);
-
-
-	flash_bitfile(uart9600_name);
-	usleep(1000);
-	system("stty -F /dev/ttyUL1 -echo");
-
-	Fduart = open("/dev/ttyUL1", O_RDWR);
-	if(Fduart < 0)
-	{
-		printf("Cannot open the UART device\n");
-		return -1;
-	}
-
-	readBytes(Fduart, 0x39, 0x00, Buffer, 3, 1);
-	for(i = 0; i<3; i++)
-	{
-		printf("%c", Buffer[i]);
-	}
-
-	for(i=0; i<20; i++)
-	{
-		readBytes(Fduart,0x39, 0x03, Buffer, 1, 1);
-		printf("\n%d", Buffer[0]);
-		usleep(100000);
-	}*/
-
-
+	GPIO_Close(leds);
+	GPIO_Close(buttons);
+	printf("\nreturn 0\n\n");
 	return 0;
 }
 
-// protocol: 0=I2C
+// protocol: 0=I2C, 1=UART
 int readBytes(int device,int i2c_addr, uint8_t adr, uint8_t result[], uint8_t count, uint8_t protocol)
 {
 	uint8_t frameBuffer[30];
 	uint8_t readBuffer[30];
+
 	//Command Write Bytes
-	frameBuffer[0] = 0x0F;
-	frameBuffer[1] = adr;
-	frameBuffer[2] = count;
+	frameBuffer[0] = 0x7E;
+	frameBuffer[1] = adr + 0x30;
+	frameBuffer[2] = count + 0x30;
+
+	// Transmit address and receive values
 	if(transRecvBytes(device, i2c_addr, frameBuffer, 3, readBuffer, count, protocol) == 0)
 	{
 		for (int i = 0; i < count; i++)
@@ -201,13 +226,14 @@ int readBytes(int device,int i2c_addr, uint8_t adr, uint8_t result[], uint8_t co
 	return 1;
 }
 
+// Transmit address and receive values
 int transRecvBytes(int device, int i2c_addr, uint8_t sendBuf[], uint8_t sendCnt, uint8_t receiveBuf[], uint8_t receiveCount, uint8_t protocol)
 {
 	transmitBytes(device, i2c_addr, sendBuf, sendCnt, protocol);
-	//usleep(10000);
 	return receiveBytes(device, i2c_addr, receiveBuf, receiveCount, protocol);
 }
 
+// Transmit
 void transmitBytes(int device, int i2c_addr, uint8_t sendBuf[], uint8_t sendCnt, Xuint8 protocol)
 {
 	int Status;
@@ -215,16 +241,20 @@ void transmitBytes(int device, int i2c_addr, uint8_t sendBuf[], uint8_t sendCnt,
 	// I2C
 	if(!protocol)
 	{
-		Status = ioctl(device, I2C_SLAVE_FORCE, i2c_addr);
+		Status = ioctl(device, I2C_SLAVE_FORCE, i2c_addr); // configure I2C address
 		if(Status < 0)
 		{
+			red();
 			printf("\n NACK\n");
+			resetcol();
 			return;
 		}
-		Status = write(device, sendBuf, sendCnt);
+		Status = write(device, sendBuf, sendCnt); // send
 		if(Status < 0)
 		{
+			red();
 			printf("\n I2C Write failed\n");
+			resetcol();
 			return;
 		}
 	}
@@ -232,15 +262,18 @@ void transmitBytes(int device, int i2c_addr, uint8_t sendBuf[], uint8_t sendCnt,
 	// UART
 	else
 	{
-		Status = write(device, sendBuf, sendCnt);
+		Status = write(device, sendBuf, sendCnt); // send
 		if(Status < 0)
 		{
+			red();
 			printf("\n UART Write failed\n");
+			resetcol();
 			return;
 		}
 	}
 }
 
+// Receive
 int receiveBytes(int device, int i2c_addr, uint8_t receiveBuf[], uint8_t cnt, Xuint8 protocol)
 {
 	int Status;
@@ -248,49 +281,62 @@ int receiveBytes(int device, int i2c_addr, uint8_t receiveBuf[], uint8_t cnt, Xu
 	//I2C
 	if(!protocol)
 	{
-		Status = ioctl(device, I2C_SLAVE_FORCE, i2c_addr);
+		Status = ioctl(device, I2C_SLAVE_FORCE, i2c_addr); // configure address
 		if(Status < 0)
 		{
+			red();
 			printf("\n NACK\n");
+			resetcol();
 			return 1;
 		}
 
-		Status = read(device, receiveBuf, cnt);
+		Status = read(device, receiveBuf, cnt); // receive
 		if(Status < 0)
 		{
+			red();
 			printf("\n I2C Read failed\n");
+			resetcol();
 			return 1;
 		}
 	}
 	else
 	{
 
-		int timeout_ms = 200;
+		int timeout_ms = 200; // Timeout: 200ms
 		int msCount = 0;
 		int bytes_available;
+
+		// UART-Timeout
 		while(msCount < timeout_ms)
 		{
-			ioctl(device, FIONREAD, &bytes_available);
+			ioctl(device, FIONREAD, &bytes_available); // detect how many bytes are available in UART
+
+			// If enough bytes are received
 			if(bytes_available >= (cnt + 1))
 			{
 				char trashBuf[1];
 				//Read bytes
-				Status = read(device, receiveBuf, cnt);
+				Status = read(device, receiveBuf, cnt); // read
 				if(Status < 0)
 				{
+					red();
 					printf("\n UART Read failed\n");
+					resetcol();
 					return 1;
 				}
-				Status = read(device, trashBuf, 1);  // Dump the 0xa
+				Status = read(device, trashBuf, 1);  // Dump received CR
 				return 0;
 			}
-			//else
-			{
-				usleep(10000);
-				msCount += 10;
-			}
+
+			usleep(10000);
+			msCount += 10; // Increase time
+
 		}
-		printf("UART Timeout");
+
+		// Timeout
+		red();
+		printf("\nUART Timeout\n");
+		resetcol();
 		return 1;
 
 	}
@@ -298,46 +344,22 @@ int receiveBytes(int device, int i2c_addr, uint8_t receiveBuf[], uint8_t cnt, Xu
 }
 
 
-int I2C_WriteByte(int device, Xuint8 addr, Xuint8 data)
+// Helper functions for visualization
+void red(void)
 {
-	int Status;
-	Xuint8 WriteBuf[1];
-	WriteBuf[0] = data;
-
-	Status = ioctl(device, I2C_SLAVE_FORCE, addr);
-	if(Status < 0)
-	{
-		printf("\n NACK\n");
-		return 1;
-	}
-
-	Status = write(device, WriteBuf, 1);
-	if(Status < 0)
-	{
-		printf("\n Write failed\n");
-		return 1;
-	}
-	return 0;
+	printf("\033[1;31m");
 }
 
-int I2C_ReadByte(int device, Xuint8 addr, Xuint8 *data)
+void resetcol(void)
 {
-	int Status;
-
-	Status = ioctl(device, I2C_SLAVE_FORCE, addr);
-	if(Status < 0)
-	{
-		printf("\n NACK\n");
-		return 1;
-	}
-
-	Status = read(device, data, 1);
-	if(Status < 0)
-	{
-		printf("\n Read failed\n");
-		return 1;
-	}
-	return 0;
+	printf("\033[0m");
 }
+
+void green(void)
+{
+	printf("\033[1;32m");
+}
+
+
 
 
